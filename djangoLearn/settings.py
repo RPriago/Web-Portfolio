2738d50c70d760cc7a23f32f8bc40af7483880a3
Rangga <rangga1874@gmail.com>
2024-07-30 17:24:41 +0700

    a change

--- djangoLearn/settings.py
+++ djangoLearn/settings.py
@@ -13,7 +13,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
